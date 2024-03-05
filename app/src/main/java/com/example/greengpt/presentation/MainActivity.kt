890f1eac36051208c495debe1bcbb4bb9af02d7a
bunyatov <bunyatovbhmn@gmail.com>
2024-03-05 16:04:33 +0400

    BottomNav is ready

--- app/src/main/java/com/example/greengpt/presentation/MainActivity.kt
+++ app/src/main/java/com/example/greengpt/presentation/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.greengpt.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.greengpt.R
 import com.example.greengpt.databinding.ActivityMainBinding
 
@@ -17,5 +16,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        setup()
+    }
+
+    private fun setup() {
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHost.navController
+
+        with(binding) {
+            bottomNav.setupWithNavController(navController)
+        }
     }
 }
